af4052e47da8c99cb6ecf94a0e241f97695164be
khanh02092006 <dinhkhanh1881@gmail.com>
2024-11-20 15:23:44 +0700

    session3_ ex03

---
+++ session3_ ex03.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main (){
+	int r;
+	scanf("%d",&r);
+	float chuvi = 2 * 3.14 * r;
+	float dientich = 3.14 * r* r;
+	printf("chu vi hinh tron là : %.2f\n ", chuvi  );
+	printf (" dien tich hinh tron là : %.2f\n", dientich);
+	return 0;
+	}
+
